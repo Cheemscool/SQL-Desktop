41e6869249277295af8016819061b04bad49ce57
Cheemscool <75301496+Cheemscool@users.noreply.github.com>
2023-01-29 00:05:21 +0100

    Update Form1.cs

--- SQL-Desktop/Form1.cs
+++ SQL-Desktop/Form1.cs
@@ -8,13 +8,23 @@ namespace SQL_Desktop
 {
     public partial class Form1 : Form
     {
+        #region Start
         List<Query> queries;
+        string defaultSettings = "<!DOCTYPE html>\r\n<html lang='it'>\r\n<head>\r\n<meta charset='UTF-8'>\r\n<title>SQLite Database</title>\r\n</head>\r\n<body>\r\n\r\n</body>\r\n</html>";
         public Form1()
         {
             InitializeComponent();
             queries = new List<Query>();
+            if (!File.Exists("default.html"))
+            {
+                StreamWriter sr = new StreamWriter("default.html");
+                sr.WriteLine(defaultSettings);
+                sr.Close();
+            }
         }
+        #endregion
 
+        #region Seleziona il DB
         private void btnPathDb_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -24,7 +34,9 @@ namespace SQL_Desktop
 
             if (dialogResult == DialogResult.OK) txtPathDB.Text = openFileDialog.FileName;
         }
+        #endregion
 
+        #region Seleziona le Query
         private void btnPathQuery_Click(object sender, EventArgs e)
         {
             string file;
@@ -57,15 +69,15 @@ namespace SQL_Desktop
             cmboxQuery.DisplayMember = "Title";
             cmboxQuery.ValueMember = "Content";
         }
+        #endregion
 
+        #region Esegue le Query prendendo come DB il file selezionato dall'utente
         private void btnRun_Click(object sender, EventArgs e)
         {
             string connectionString = $"Data Source={txtPathDB.Text};Version=3", queryContent = txtSQL.Text;
-
             queryContent = ApplyPar(queryContent);
 
             DataTable dt = new DataTable();
-
             SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(queryContent, connectionString);
 
             try
@@ -75,7 +87,9 @@ namespace SQL_Desktop
             }
             catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
+        #endregion
 
+        #region Metodo per dare determinati parametri a certe query
         private string ApplyPar(string input)
         {
             if (txtFirstPar.Text != "") input = input.Replace("//P1//", txtFirstPar.Text);
@@ -87,7 +101,9 @@ namespace SQL_Desktop
 
             return input;
         }
+        #endregion
 
+        #region Decodifica la lista delle Query
         private List<Query> DecodeFile(string input)
         {
             List<Query> output = new List<Query>();
@@ -111,13 +127,17 @@ namespace SQL_Desktop
             }
             return output;
         }
+        #endregion
 
+        #region Aggiorna la Text Box quando viene scelta un'altra query
         private void cmboxQuery_SelectedIndexChanged(object sender, EventArgs e)
         {
             var query = (Query)cmboxQuery.SelectedItem;
             if (query != null) { txtSQL.Text = query.Content; }
         }
+        #endregion
 
+        #region Esporta il DB come file HTML
         private void btnExport_Click(object sender, EventArgs e)
         {
             HtmlAgilityPack.HtmlDocument html = new HtmlAgilityPack.HtmlDocument();
@@ -126,7 +146,8 @@ namespace SQL_Desktop
             sd.Title = "Scegli il percorso";
             sd.FileName = "Database";
             if (sd.ShowDialog() == DialogResult.OK) html.Save(sd.FileName);
-            html.Text = "Test";
+            //html.LoadHtml(...);
         }
+        #endregion
     }
 }
